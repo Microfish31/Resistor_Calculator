66c86509bb93e0ec8f167545dd939f008097dacb
LAPTOP-SO8HJF28\Yang <czsea35@yahoo.com.tw>
2021-05-29 13:32:59 +0800

    add file

---
+++ Resistor/Resistor/Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿
+namespace Resistor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.R1 = new System.Windows.Forms.TextBox();
+            this.R2 = new System.Windows.Forms.TextBox();
+            this.R4 = new System.Windows.Forms.TextBox();
+            this.R3 = new System.Windows.Forms.TextBox();
+            this.R6 = new System.Windows.Forms.TextBox();
+            this.R5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "送出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "黑",
+            "棕",
+            "紅",
+            "橘",
+            "黃",
+            "綠",
+            "藍",
+            "紫",
+            "灰",
+            "白"});
+            this.comboBox1.Location = new System.Drawing.Point(98, 153);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "選擇顏色";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(98, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 23);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "四環";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton1_MouseClick);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(239, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 23);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "五環";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton2_MouseClick);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(385, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 23);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "六環";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton3_MouseClick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "黑",
+            "棕",
+            "紅",
+            "橘",
+            "黃",
+            "綠",
+            "藍",
+            "紫",
+            "灰",
+            "白"});
+            this.comboBox2.Location = new System.Drawing.Point(98, 237);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 27);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "選擇顏色";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "黑",
+            "棕",
+            "紅",
+            "橘",
+            "黃",
+            "綠",
+            "藍",
+            "紫",
+            "灰",
+            "白"});
+            this.comboBox3.Location = new System.Drawing.Point(98, 314);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 27);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.Text = "選擇顏色";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "黑",
+            "棕",
+            "紅",
+            "橘",
+            "黃",
+            "綠",
+            "藍",
+            "紫",
+            "灰",
+            "白",
+            "金",
+            "銀"});
+            this.comboBox4.Location = new System.Drawing.Point(340, 153);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 27);
+            this.comboBox4.TabIndex = 7;
+            this.comboBox4.Text = "選擇顏色";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "棕",
+            "紅",
+            "綠",
+            "藍",
+            "紫",
+            "灰",
+            "金",
+            "銀"});
+            this.comboBox5.Location = new System.Drawing.Point(340, 232);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(151, 27);
+            this.comboBox5.TabIndex = 8;
+            this.comboBox5.Text = "選擇顏色";
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "棕",
+            "紅",
+            "橘",
+            "黃",
+            "藍",
+            "紫"});
+            this.comboBox6.Location = new System.Drawing.Point(340, 314);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(151, 27);
+            this.comboBox6.TabIndex = 9;
+            this.comboBox6.Text = "選擇顏色";
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "第一環";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "第二環";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "第三環";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(340, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "乘數";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(340, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "誤差";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(340, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 19);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "PPM";
+            // 
+            // R1
+            // 
+            this.R1.Location = new System.Drawing.Point(577, 153);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(10, 27);
+            this.R1.TabIndex = 16;
+            // 
+            // R2
+            // 
+            this.R2.Location = new System.Drawing.Point(603, 153);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(10, 27);
+            this.R2.TabIndex = 17;
+            // 
+            // R4
+            // 
+            this.R4.Location = new System.Drawing.Point(659, 153);
+            this.R4.Name = "R4";
+            this.R4.Size = new System.Drawing.Size(10, 27);
+            this.R4.TabIndex = 18;
+            // 
+            // R3
+            // 
+            this.R3.Location = new System.Drawing.Point(619, 153);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(10, 27);
+            this.R3.TabIndex = 19;
+            // 
+            // R6
+            // 
+            this.R6.Location = new System.Drawing.Point(758, 153);
+            this.R6.Name = "R6";
+            this.R6.Size = new System.Drawing.Size(10, 27);
+            this.R6.TabIndex = 20;
+            // 
+            // R5
+            // 
+            this.R5.Location = new System.Drawing.Point(709, 153);
+            this.R5.Name = "R5";
+            this.R5.Size = new System.Drawing.Size(10, 27);
+            this.R5.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(598, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 19);
+            this.label7.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(638, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 19);
+            this.label8.TabIndex = 23;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(688, 233);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 19);
+            this.label9.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(638, 331);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 19);
+            this.label10.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 468);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.R5);
+            this.Controls.Add(this.R6);
+            this.Controls.Add(this.R3);
+            this.Controls.Add(this.R4);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Resistor Value";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox R1;
+        private System.Windows.Forms.TextBox R2;
+        private System.Windows.Forms.TextBox R4;
+        private System.Windows.Forms.TextBox R3;
+        private System.Windows.Forms.TextBox R6;
+        private System.Windows.Forms.TextBox R5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Resistor/Resistor/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Resistor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+            comboBox3.Visible = false;
+            label3.Visible = false;
+            R3.Visible = false;
+            R5.Visible = false;
+            label6.Visible = false;
+            comboBox6.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comboBox1.BackColor = Color_For_Ring(comboBox1.SelectedIndex);
+            R1.BackColor = Color_For_Ring(comboBox1.SelectedIndex);
+        }
+
+        string[] multiple = {"* 1", "* 10", "* 100", "* 1k", "* 10k", "* 100k", "* 1M", "* 10M", "* 100M", "* 1G", "* 0.1", "* 0.01" };
+
+        string[] error = {"1%", "2%", "0.5%", "0.25%", "0.1%", "0.05%", "5%", "10%"};
+
+        string[] ppm = { "100 ppm", "50 ppm", "15 ppm", "25 ppm", "10 ppm", "5 ppm", };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double resistance;
+            if (radioButton1.Checked)
+            {
+                resistance = (comboBox1.SelectedIndex * 10 + comboBox2.SelectedIndex);
+                label7.Text = resistance.ToString();
+                label8.Text = error[comboBox5.SelectedIndex];
+                label9.Text = multiple[comboBox4.SelectedIndex];
+            }
+            else if (radioButton2.Checked)
+            {
+                resistance = (comboBox1.SelectedIndex * 100 + comboBox2.SelectedIndex*10 + comboBox3.SelectedIndex);
+                label7.Text = resistance.ToString();
+                label8.Text = error[comboBox5.SelectedIndex];
+                label9.Text = multiple[comboBox4.SelectedIndex];
+            }
+            else {
+                resistance = (comboBox1.SelectedIndex * 100 + comboBox2.SelectedIndex * 10 + comboBox3.SelectedIndex);
+                label7.Text = resistance.ToString();
+                label8.Text = error[comboBox5.SelectedIndex];
+                label9.Text = multiple[comboBox4.SelectedIndex];
+                label10.Text = ppm[comboBox6.SelectedIndex];
+            }
+        }
+
+        private void radioButton1_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBox3.Visible = false;
+            label3.Visible = false;
+            R3.Visible = false;
+            R5.Visible = false;
+            label6.Visible = false;
+            comboBox6.Visible = false;
+        }
+
+        private void radioButton2_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBox3.Visible = true;
+            label3.Visible = true;
+            R3.Visible = true;
+            R5.Visible = false;
+            label6.Visible = false;
+            comboBox6.Visible = false;
+        }
+
+        private void radioButton3_MouseClick(object sender, MouseEventArgs e)
+        {
+            comboBox3.Visible = true;
+            label3.Visible = true;
+            R3.Visible = true;
+            R5.Visible = true;
+            label6.Visible = true;
+            comboBox6.Visible = true;
+        }
+
+        private void comboBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private Color Color_For_Ring(int number) {
+            switch (number) {
+                case 0: return Color.Black;
+                case 1: return Color.Brown;
+                case 2: return Color.Red;
+                case 3: return Color.Orange;
+                case 4: return Color.Yellow;
+                case 5: return Color.Green;
+                case 6: return Color.Blue;
+                case 7: return Color.Purple;
+                case 8: return Color.Gray;
+                default: return Color.White;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            R2.BackColor = Color_For_Ring(comboBox2.SelectedIndex);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            R3.BackColor = Color_For_Ring(comboBox3.SelectedIndex);
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color color;
+            switch (comboBox4.SelectedIndex)
+            {
+                case 0: color = Color.Black; break;
+                case 1: color = Color.Brown; break;
+                case 2: color = Color.Red; break;
+                case 3: color = Color.Orange; break;
+                case 4: color = Color.Yellow; break;
+                case 5: color = Color.Green; break;
+                case 6: color = Color.Blue; break;
+                case 7: color = Color.Purple; break;
+                case 8: color = Color.Gray; break;
+                case 9: color = Color.White; break;
+                case 10: color = Color.Gold; break;
+                default: color = Color.Silver; break;
+            }
+            R4.BackColor = color;
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color color;
+            switch (comboBox5.SelectedIndex)
+            {
+                case 0: color = Color.Brown; break;
+                case 1: color = Color.Red; break;
+                case 2: color = Color.Green; break;
+                case 3: color = Color.Blue; break;
+                case 4: color = Color.Purple; break;
+                case 5: color = Color.Gray; break;
+                case 6: color = Color.Gold; break;
+                default: color = Color.Silver; break;
+            }
+            R6.BackColor = color;
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Color color;
+            switch (comboBox6.SelectedIndex)
+            {
+                case 0: color = Color.Brown; break;
+                case 1: color = Color.Red; break;
+                case 2: color = Color.Orange; break;
+                case 3: color = Color.Yellow; break;
+                case 4: color = Color.Blue; break;
+                default: color = Color.Purple; break;
+            }
+            R5.BackColor = color;
+        }
+    }
+}
